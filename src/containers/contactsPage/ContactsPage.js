c97c03a4c7fcc5e4bbca833b189e2cec600b737b
Rachid Amrani <rachid3alaoui@gmail.com>
2021-05-14 16:20:39 +0000

    add local states and render ContactForm

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ContactForm } from '../../components/contactForm/ContactForm'
 
-export const ContactsPage = () => {
+export const ContactsPage = ({ contacts, addNewContact }) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [contactName, setContactName] = useState(null)
+  const [contactPhone, setcontactPhone] = useState(null)
+  const [contactEmail, setcontactEmail] = useState(null)
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -23,10 +27,25 @@ export const ContactsPage = () => {
     <div>
       <section>
         <h2>Add Contact</h2>
+        {
+          // Check for duplicates whenever the name in the form changes and indicate the name is a duplicate
+          // Only add a new contact on form submission if it does not duplicate an existing contact’s name
+          // A successful submission should clear the form
+        }
+        <ContactForm
+          name={contactName}
+          setName={setContactName}
+          phone={contactPhone}
+          setPhone={setcontactPhone}
+          email={contactEmail}
+          setEmail={setcontactEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contact={contacts} />
       </section>
     </div>
   )
